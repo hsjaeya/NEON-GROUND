36557397e2329ab25978fc002d1ae889b4c66434
Seongjae <141992140+hsjaeya@users.noreply.github.com>
2026-04-07 17:09:14 +0900

    feat(frontend): Vercel Analytics 추가

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+import { inject } from "@vercel/analytics";
 import "./index.css";
 import App from "./App.tsx";
+
+inject();
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
